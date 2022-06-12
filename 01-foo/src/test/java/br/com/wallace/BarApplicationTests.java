9c4e592369d86372a7dc92c45a78d657c96e2665
Wallace <wallace.caldeira@cwi.com.br>
2022-06-12 12:20:05 -0300

    Initial commit

---
+++ 01-foo/src/test/java/br/com/wallace/BarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wallace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
